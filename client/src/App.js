6e88573a1d6818f522a317a86a0e3d6614332880
Abir Taheer <abir488@gmail.com>
2019-09-28 11:17:35 -0400

    added router and landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import {Route, BrowserRouter as Router} from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Router>
+        <div>
+          <Route exact path="/" component={Landing}/>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/home/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Navbar} from "../Navbar";
+
+export class Landing extends React.Component {
+    render(){
+        return (
+            <div>
+                <Navbar />
+            </div>
+        )
+    }
+}
